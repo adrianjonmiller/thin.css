7b7e88cc7c18588db5c9135ce7f9dc8671c59bf6
Adrian Miller <adrianjonmiller@gmail.com>
2018-04-03 21:01:09 -0700

    updated dist to thin.css

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,7 +1,9 @@
 import files from './files';
 import paths from './paths';
 import sass from 'gulp-sass';
+import rename from 'gulp-rename';
 import gulp from 'gulp';
+import path from 'path';
 import browserSync from 'browser-sync';
 
 export default class Sass {
@@ -19,6 +21,7 @@ export default class Sass {
   thinBuild () {
     return gulp.src(files.sass)
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename('thin.css'))
       .pipe(gulp.dest(paths.dist))
   }
 }
