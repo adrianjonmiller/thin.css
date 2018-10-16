4dd6a4f041f8be5b501fee82fd324c485fd36f07
Adrian Miller <adrianjonmiller@gmail.com>
2018-10-15 21:18:32 -0700

    pushing thin-scss to heroku

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const sass = require('node-sass')
+const path = require('path')
+
+const port = 3000
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/debug/thin.css', (req, res) => {
+  var version = req.query.version || 'default'
+
+  sass.render({
+    file: path.resolve(__dirname, `src/${version}/debug.scss`)
+  }, function (err, result) {
+    if (err) {
+      throw err
+    }
+
+    res.setHeader('Content-Transfer-Encoding', 'binary');
+    res.setHeader('Content-Type', 'text/css');
+    res.send(new Buffer(result.css, 'binary'))
+  })
+})
+
+app.get('/main.css', (req, res) => {
+  var version = req.query.version || 'default'
+
+  sass.render({
+    file: path.resolve(__dirname, `public/styles/main.scss`)
+  }, function (err, result) {
+    if (err) {
+      throw err
+    }
+
+    res.setHeader('Content-Transfer-Encoding', 'binary');
+    res.setHeader('Content-Type', 'text/css');
+    res.send(new Buffer(result.css, 'binary'))
+  })
+})
+
+app.get('/cdn/thin.css', (req, res) => {
+  var version = req.query.version || 'default'
+
+  sass.render({
+    file: path.resolve(__dirname, `src/${version}/index.scss`)
+  }, function(err, result) {
+    if (err) {
+      throw err 
+    }
+
+    res.setHeader('Content-Transfer-Encoding', 'binary');
+    res.setHeader('Content-Type', 'text/css');
+    res.send(new Buffer(result.css, 'binary'))
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'public/index.html'));
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
--- gulp/files.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-import paths from './paths';
-
-export default {
-  config: path.join(paths.docs, '_config.yml'),
-  sass: path.join(paths.src, '**/*.scss')
-}
