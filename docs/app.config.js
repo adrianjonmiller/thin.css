66de05f7c5da5aad7b35d3f20158d0850bbb2ba5
Adrian Miller <adrianjonmiller@gmail.com>
2021-02-26 09:11:46 -0600

    added doc env to docs

--- docs/app.config.js
+++ docs/app.config.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const nunjucks = require('nunjucks');
 const fetch = require('node-fetch');
 const path = require('path');
 const stringToReadableURL = require("string-to-readable-url");
 const markdownFilter = require('nunjucks-markdown-filter');
-const token = '30d8c651eebea64a994dcba84ddd9d';
+const token = process.env.DATO_TOKEN;
 const query = require('./query')
 
 let env = nunjucks.configure('./views', {
